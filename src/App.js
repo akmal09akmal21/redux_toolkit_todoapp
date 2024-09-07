9bc72ca89844fbc3acf1a05d9f5de007d3ba834a
akmal09akmal21 <takmal819@gmail.com>
2024-09-07 17:45:45 +0500

    redux toolkit todo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-// import { Route, Routes } from "react-router-dom";
-// import AddStudent from "./Components/AddStudent";
-// import Students from "./Components/Students";
-// import EditStudent from "./Components/EditStudent";
 import AddTask from "./components/AddTask";
 import TaskList from "./components/TaskList";
 const App = () => {
@@ -9,11 +5,6 @@ const App = () => {
     <>
       <AddTask />
       <TaskList />
-      {/* <Routes>
-        <Route path="/students/create" element={<AddStudent />} />
-        <Route path="/" element={<Students />} />
-        <Route path="/students/edit/:id" element={<EditStudent />} />
-      </Routes> */}
     </>
   );
 };
